84fb42d183b127b0aae5ae94d0a67c857c80e1ea
JanHryb <jan.hryb@gmail.com>
2023-01-29 21:50:34 +0100

    up to date commit

--- server/models/TaskCategory.js
+++ server/models/TaskCategory.js
@@ -3,6 +3,7 @@ const { Schema } = require("mongoose");
 
 const taskCategorySchema = new Schema({
   name: { type: String, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 const TaskCategory = mongoose.model("TaskCategory", taskCategorySchema);
